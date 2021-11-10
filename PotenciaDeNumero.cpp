99fca338502576f4c17b01a7127374df6a30e6df
Ariel <33358756+Ariel210@users.noreply.github.com>
2021-11-10 18:45:17 -0300

    Exercise math. C++

---
+++ PotenciaDeNumero.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main ()
+{
+    int numero;
+    cout<<"Ingrese num: ";
+    cin>>numero;
+
+    cout<<"Numero: "<<numero<<"\n";
+    cout<<"Numero con potencia: "<<pow(numero,2);
+
+    return 0;
+}

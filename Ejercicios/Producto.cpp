63d880b643e0748cd436f5dd961a8908464675ce
Ariel Izurieta <ariel_izurieta@hotmail.com>
2022-04-23 12:57:02 -0300

    Other Exercise 2 - C++

---
+++ Ejercicios/Producto.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    int numero1;
+    int numero2;
+
+    cout<<"1er num: ";
+    cin>>numero1;
+    cout<<"2do num: ";
+    cin>>numero2;
+
+    cout<<"Resultado del producto: "<<numero1*numero2;
+
+    return 0;
+}

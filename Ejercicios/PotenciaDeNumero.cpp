d62699bc3061deac75a7800ec1c5d9eba58149ee
Ariel Izurieta <izurieta811@gmail.com>
2022-04-23 10:35:50 -0300

    Other Exercise. C++

---
+++ Ejercicios/PotenciaDeNumero.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main ()
+{
+    //Numero Agregado.
+    //Numero Positivo
+    int numero;
+    cout<<"Ingrese num: ";
+    cin>>numero;
+
+    cout<<"Numero: "<<numero<<"\n";
+    cout<<"Numero con potencia: "<<pow(numero,2);
+
+    return 0;
+}
